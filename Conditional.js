4fc38257132efe7209b8f4ee6f8efd1f74c275fe
Sudhakar21042001 <sudhakar@wissend.com>
2024-11-26 18:40:37 +0530

    Functions and Conditional added

---
+++ Conditional.js
@@ -0,0 +1,60 @@
+/*
+var rain=true
+if(rain){
+    console.log("Take an Umberla")
+}else{
+    console.log("Enjoy the sunshine")
+}
+var homework=false
+if(homework){
+    console.log("Great Job!")
+}else{
+    console.log("Finish Your homework")
+}
+var Cookies=true
+if(Cookies){
+    console.log("Would you like a cookie")
+}else{
+    console.log("Time to bake more cookies")
+}
+var brush=false
+if(brush){
+    console.log("Your teeth are clean")
+}else{
+    console.log("Go brush your teeth now")
+}
+var birthday=false
+if(birthday){
+    console.log("Happy birthday")
+}else{
+    console.log("Advance Happy birthday")
+}
+
+*/
+/*
+var season = "spring"
+if (season == "spring") {
+    console.log("Enjoy the blomming flowers")
+} if (season == "summer") {
+    console.log("Have fun in the sun")
+} if (season == "autumn") {
+    console.log("Admire the colourful leaves")
+} if (season == "winter") {
+    console.log("Bundle up and stay warm")
+} */
+
+  /*  var score = 34
+    if(score<50){
+        console.log("You need to improve")
+    }else if(score>50 && score<=70){
+        console.log("Good")
+    }else if(score>70){
+        console.log("Excellent")
+    }  */
+
+    var a=11
+    if(a%2==0){
+        console.log("Prime number")
+    }else{
+        console.log("Not a prime number")
+    }
---
+++ Functions.js
@@ -0,0 +1,56 @@
+
+/* function Oppo()
+{
+    console.log("This is Oppo")
+}
+
+function apple()
+{
+    console.log("This is apple")
+}
+function redmi()
+{
+    console.log("This is redmi")
+}
+Oppo()
+redmi()
+apple()  */
+/*
+var a=10 ,b=20;
+function add()
+{
+  console.log(a+b);
+}
+add() */
+
+/*
+var factor = "Kamal" , fplayer ="Dhoni" , fmovie = "Anbe sivam"
+function favourite()
+{
+   console.log("Favourite Actor  : "+factor)
+   console.log("Favourite Player : "+fplayer)
+   console.log("Favourite Movie  : "+fmovie)
+}
+favourite()
+*/
+/*
+function area(length,breadth)
+{
+  console.log("Area is: "+length*breadth)
+}
+area(10,20)  */
+
+/*
+function myname()
+{
+    return "John"
+}
+ var a=myname()
+ console.log(a)
+ */
+function add(a,b)
+{
+    return a+b
+}
+var c=add(30,20)
+console.log(c)
